894a9ecb0c281e6ad2b9ce585540e4b8b7bca295
Leomar <leomar.sartor@funcionalcorp.com.br>
2021-12-30 13:42:27 -0300

    Troca de mensagem e teste dos artefatos

--- MyFirstContinuousIntegration/MyFirstContinuousIntegration/Program.cs
+++ MyFirstContinuousIntegration/MyFirstContinuousIntegration/Program.cs
@@ -6,7 +6,7 @@ namespace MyFirstContinuousIntegration
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World Jenkins!");
 
             Console.ReadKey();
         }
